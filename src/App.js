8f10c0b13966b31fba1fb0f0e3990079cd87d3a3
unknown <go_frioui@esi.dz>
2021-04-14 14:34:33 +0100

    Fixing styles

--- src/App.js
+++ src/App.js
@@ -204,7 +204,9 @@ function App() {
       {user?.displayName ? (
         <ImageUpload username={user.displayName} />
       ) : (
-        <h3>Sorry you need to login to upload</h3>
+        <center>
+          <h3>Sorry you need to login to upload</h3>
+        </center>
       )}
     </div>
   );
